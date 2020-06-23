81ef509337b7da20e9f63358b965eae4de8a9cc9
soehina <kamuhina@gmail.com>
2020-06-24 01:06:11 +0900

    model/migrate

---
+++ server/database/migrations/2020_06_23_160054_create_contact_forms_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactFormsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contact_forms', function (Blueprint $table) {
+            $table->id();
+            $table->char('name', 50);
+            $table->string('title', 100);
+            $table->string('email', 100);
+            $table->boolean('gender');
+            $table->text('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contact_forms');
+    }
+}
